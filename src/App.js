01aadbc1474299252d4054c589a52ef0c6bcae40
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2025-02-09 18:05:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/tasks";
+const API_URL = "https://todo-app-backend-eight-pi.vercel.app/";
 
 function App() {
   const [tasks, setTasks] = useState([]);
